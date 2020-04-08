013e0271ac4818a2702efceb435079d76d18ebbb
simon <sjtucker28@gmail.com>
2020-04-08 09:33:08 +0100

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,85 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const API = 'http://localhost:3000/stocks'
+
+
 class MainContainer extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      stocks: [],
+      portfolioStocks: [],
+      displayStocks: []
+    } 
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({
+      stocks: data,
+      displayStocks: data
+      })
+    )}
+
+    addToPortfolio = stock => {
+      this.setState({
+        portfolioStocks: [...this.state.portfolioStocks, stock]
+      })
+    }
+
+    removeStock = (stock) => {
+      this.setState({
+        portfolioStocks: this.state.portfolioStocks.filter(s => s !== stock)
+      })
+    }
+
+    filterStocks = (type) => {
+
+      if(type !== "All"){
+        this.setState({
+          displayStocks: this.state.stocks.filter(stock => stock.type === type)        
+        })
+      }else{
+        this.setState({
+          displayStocks: this.state.stocks
+        })
+      }
+    }
+  
+    sortStocks = (sortBy) => {
+      let arr = []
+      switch(sortBy){
+        case "Alphabetically":
+          arr = this.state.displayStocks.sort((a,b) => a.name > b.name ? 1 : -1)
+          break;
+        case "Price":
+            arr = this.state.displayStocks.sort((a,b) => a.price > b.price ? 1 : -1)
+          break;
+        default:
+          console.log("Wrong choice")
+      }
+      this.setState({
+        displayStocks: arr
+      })
+    }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar filterStocks={this.filterStocks} sortStocks={this.sortStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.displayStocks} addToPortfolio={this.addToPortfolio} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portfolioStocks} removeStock={this.removeStock}/>
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -9,6 +9,7 @@ class PortfolioContainer extends Component {
         <h2>My Portfolio</h2>
           {
             //render your portfolio stocks here
+            this.props.stocks.map( stock => <Stock stock={stock} key={stock.id} removeStock={this.props.removeStock}/>)
           }
       </div>
     );
